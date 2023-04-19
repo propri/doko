a0bd646f821db905342fab1b33788109bfb185cc
Wilhelm Mailaender <wilhelm@mailaender-home.de>
2023-04-19 23:03:43 +0200

    Karten Reihenfolge vom Standardspiel definiert

--- backend/game.ts
+++ backend/game.ts
@@ -31,6 +31,73 @@ const defaultTrumpf: Trumpf = {
   werte: ['Dame', 'Bube'],
 }
 
+export const defaultTrumpfOrder = {
+  'Herz 10': 5,
+  'Kreuz Dame': 6,
+  'Pik Dame': 7,
+  'Herz Dame': 8,
+  'Karo Dame': 9,
+  'Kreuz Bube': 10,
+  'Pik Bube': 11,
+  'Herz Bube': 12,
+  'Karo Bube': 13,
+  'Karo Ass': 14,
+  'Karo 10': 15,
+  'Karo Koenig': 16,
+  'Karo 9': 17,
+}
+
+export const defaultKreuzOrder = {
+  'Kreuz Ass': 5,
+  'Kreuz 10': 6,
+  'Kreuz Koenig': 7,
+  'Kreuz 9': 6,
+}
+
+export const defaultPikOrder = {
+  'Pik Ass': 5,
+  'Pik 10': 6,
+  'Pik Koenig': 7,
+  'Pik 9': 6,
+}
+
+export const defaultHerzOrder = {
+  'Herz Ass': 5,
+  'Herz Koenig': 6,
+  'Herz 9': 7,
+}
+
+export const displayOrder = {
+  // Trumpf
+  'Herz 10': 5,
+  'Kreuz Dame': 6,
+  'Pik Dame': 7,
+  'Herz Dame': 8,
+  'Karo Dame': 9,
+  'Kreuz Bube': 10,
+  'Pik Bube': 11,
+  'Herz Bube': 12,
+  'Karo Bube': 13,
+  'Karo Ass': 14,
+  'Karo 10': 15,
+  'Karo Koenig': 16,
+  'Karo 9': 17,
+  // Kreuz
+  'Kreuz Ass': 25,
+  'Kreuz 10': 26,
+  'Kreuz Koenig': 27,
+  'Kreuz 9': 26,
+  // Pik
+  'Pik Ass': 35,
+  'Pik 10': 36,
+  'Pik Koenig': 37,
+  'Pik 9': 36,
+  // Herz
+  'Herz Ass': 45,
+  'Herz Koenig': 46,
+  'Herz 9': 47,
+}
+
 const DamenSoloTrump: Trumpf = {
   karten: [],
   farben: [],
