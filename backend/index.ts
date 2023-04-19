1afa14d2d1e70fdbeed2079e7fdec3fc4db44ef8
Wilhelm Mailaender <wilhelm@mailaender-home.de>
2023-04-19 23:08:42 +0200

    Game in backend.ts erstellen, Karten für gewählten Spieler ausliefern

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 //const sessions = require('sessions')
 
+import { startGame, Position } from './game'
+import type { Spieler } from './game'
+
 //const SECRET = process.env.SECRET || 'supersecret-secret'
 const PORT = process.env.PORT || 3001
 
@@ -17,6 +20,44 @@ const app = express()
 //},
 //resave: false,
 //})
+
+
+const spieler: Spieler[] = [
+  {
+    name: 'Spieler A',
+    position: Position.A,
+    cards: [],
+    geber: false,
+  },
+  {
+    name: 'Spieler B',
+    position: Position.B,
+    cards: [],
+    geber: false,
+  },
+  {
+    name: 'Spieler C',
+    position: Position.C,
+    cards: [],
+    geber: false,
+  },
+  {
+    name: 'Spieler D',
+    position: Position.D,
+    cards: [],
+    geber: false,
+  },
+]
+
+const game = startGame(spieler)
+
+app.get('/my-cards', (req, res) => {
+  const currentPlayer: Spieler = game.spieler.find((s) => {
+    return req.query.spieler === s.name}
+  ) as unknown as Spieler
+  //console.log(currentPlayer)
+  res.json(currentPlayer.cards)
+})
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Foobar' })
